3f6c6e874f86812cdaa97c34736844e6c85b089e
idillon <io.daza-dillon@savoirfairelinux.com>
2023-05-09 19:21:08 -0400

    Add basic forwarding of otp request

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@
 import axios from 'axios'
 import 'dotenv/config'
 import express from 'express'
+import type { Request } from 'express'
+
+if (process.env['TAXI_API_KEY'] === undefined) {
+  throw new Error('TAXI_API_KEY is undefined. Please set it in .env file.')
+}
+const taxiApiKey: string = process.env['TAXI_API_KEY']
+
+if (process.env['OTP_ADDRESS'] === undefined) {
+  throw new Error('OTP_ADDRESS is undefined. Please set it in .env file.')
+}
+const otpAddress: string = process.env['OTP_ADDRESS']
 
 interface Coordinates {
   lat: string
@@ -39,18 +50,24 @@ const app = express()
 const getTaxiPricing = async (data: TaxiApiRequestData): Promise<TaxiApiRequestData> => {
   const response = await axios.post('https://taximtl.ville.montreal.qc.ca/api/inquiry', data, {
     headers: {
-      'X-API-KEY': process.env['TAXI_API_KEY']
+      'X-API-KEY': taxiApiKey
     }
   })
-  console.log('response', response.data)
+  return response.data
+}
+
+const getOtpResponse = async (req: Request): Promise<any> => {
+  const response = await axios.get(`${otpAddress}${req.url}`)
   return response.data
 }
 
 app.get('*', async (req, res) => {
   console.log('Request', req.method, req.url, req.protocol, req.hostname)
   try {
-    const taxiPrincing = await getTaxiPricing(data)
-    res.send(taxiPrincing)
+    const taxiPricing = await getTaxiPricing(data)
+    const otpResponse = await getOtpResponse(req)
+    console.log('taxiPricing', taxiPricing)
+    res.send(otpResponse)
   } catch (error) {
     res.send(error)
   }
