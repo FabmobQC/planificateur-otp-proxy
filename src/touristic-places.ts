daa5db8025eb039b72ec2bc951b8660624f0bd86
idillon <io.daza-dillon@savoirfairelinux.com>
2024-02-23 17:33:57 -0500

    Filter out touristic places without name

--- src/touristic-places.ts
+++ src/touristic-places.ts
@@ -2,6 +2,7 @@ import { loadJsonFile } from './file-tools.js'
 
 const touristicPlacesJson = loadJsonFile('./touristic_places/OSM_Tourism_Quebec.json') as any
 const touristicPlaces = touristicPlacesJson.features.map((feature: any) => feature.properties)
+  .filter((property: any) => property.name !== undefined)
 
 export const handleTouristicPlacesRequest = async (req: unknown): Promise<unknown[]> => {
   return touristicPlaces
