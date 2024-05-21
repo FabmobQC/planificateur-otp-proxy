22b5c65789abe1b3625e604fc59946cf7aeaed54
idillon <io.daza-dillon@savoirfairelinux.com>
2024-05-21 16:57:28 -0400

    Update touristic places

--- src/touristic-places.ts
+++ src/touristic-places.ts
@@ -2,7 +2,6 @@ import { loadJsonFile } from './file-tools.js'
 
 const touristicPlacesJson = loadJsonFile('./touristic_places/OSM_Tourism_Quebec.json') as any
 const touristicPlaces = touristicPlacesJson.features.map((feature: any) => feature.properties)
-  .filter((property: any) => property.name !== undefined)
 
 export const handleTouristicPlacesRequest = async (req: unknown): Promise<unknown[]> => {
   return touristicPlaces
