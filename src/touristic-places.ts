cd53f1a00b36efa7564f41545c4f9292ed8e821d
idillon <io.daza-dillon@savoirfairelinux.com>
2024-06-11 16:43:02 -0400

    Move touristic places to data folder

--- src/touristic-places.ts
+++ src/touristic-places.ts
@@ -1,6 +1,6 @@
 import { loadJsonFile } from './file-tools.js'
 
-const touristicPlacesJson = loadJsonFile('./touristic_places/OSM_Tourism_Quebec.json') as any
+const touristicPlacesJson = loadJsonFile('./data/quebec_touristic_places.json') as any
 const touristicPlaces = touristicPlacesJson.features.map((feature: any) => feature.properties)
 
 export const handleTouristicPlacesRequest = async (req: unknown): Promise<unknown[]> => {
