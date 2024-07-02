6482ebe5f0d1027c596937053c34163d28140939
idillon <io.daza-dillon@savoirfairelinux.com>
2024-07-02 18:19:03 -0400

    Add Navette Nature destinations

--- src/touristic-places.ts
+++ src/touristic-places.ts
@@ -1,7 +1,11 @@
 import { loadJsonFile } from './file-tools.js'
 
 const touristicPlacesJson = loadJsonFile('./data/quebec_touristic_places.json') as any
-const touristicPlaces = touristicPlacesJson.features.map((feature: any) => feature.properties)
+const navetteNatureDestinationsJson = loadJsonFile('./data/navette_nature_destinations.json') as any
+const touristicPlaces = [
+  ...touristicPlacesJson.features.map((feature: any) => feature.properties),
+  ...navetteNatureDestinationsJson
+]
 
 export const handleTouristicPlacesRequest = async (req: unknown): Promise<unknown[]> => {
   return touristicPlaces
